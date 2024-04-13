cdc45ba8f2179f5c7b9f5c216b4704cbac7e27c8
Roman <enatik14@gmail.com>
2024-04-13 23:57:44 +0300

    iterate through each folder and each file

--- pre_process.py
+++ pre_process.py
@@ -2,7 +2,20 @@ import numpy as np
 import pandas as pd
 import os
 from scipy.signal import decimate, butter, sosfilt
+import matplotlib.pyplot as plt
+
+path = "C:/flashka_back/data/with_amplifier/"
 
 dt = 20e-9 #time sample is 20 ns
 fs = 1 / dt #sampling frequency
 
+folders = os.listdir(path)
+item_idx = 0
+
+for folder in folders:
+    files = os.listdir(path + folder)
+    for item in files:
+        df = pd.read_csv(path + folder + '/' + item)
+        ch1 = df['CH1V'].to_numpy()
+        
+        
