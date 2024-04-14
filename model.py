42ac82d3a03a54257703f73f7c8ab62964895831
Roman Kozin <enatik14@gmail.com>
2024-04-15 00:36:33 +0300

    first ver

--- model.py
+++ model.py
@@ -4,6 +4,24 @@ import torch.nn as nn
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init_()
+        nn.Sequential(
+            nn.Conv2d(2, 256, 13, stride=1, padding='same'),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(),
+            nn.Conv2d(256, 128, 11, stride=1, padding='same'),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.Conv2d(128, 64, 9, stride=1, padding='same'),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+            nn.Conv2d(64, 32, 7, stride=1, padding='same'),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
+            nn.Conv2d(32, 16, 5, stride=1, padding='same'),
+            nn.BatchNorm2d(16),
+            nn.LeakyReLU(),
+            nn.Conv2d(16, 2, 3, stride=1, padding=(0, 1))
+        )
 
     def forward(x):
         ...
