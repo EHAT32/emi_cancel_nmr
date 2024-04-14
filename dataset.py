18f19a2ac351ee5c24290528083dd6f84884b0dd
Roman Kozin <enatik14@gmail.com>
2024-04-15 00:13:10 +0300

    created script for making a torch dataset

---
+++ dataset.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.data import Dataset
+import os
+
+class SkinDataset(Dataset):
+    def __init__(self, root, device):
+        self.root = root
+        if self.root[-1] != '/':
+            self.root += '/'
+        self.data = os.listdir(root)
+        self.device = device
+        
+    def __getitem__(self, index):
+        data = torch.load(self.root + str(index) + '.pt')
+        features = data[:, :2, :]
+        target = data[:, 2, :]
+        return features, target
