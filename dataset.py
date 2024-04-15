440afdbef8dd937aab15750331459138822a28ae
Roman Kozin <enatik14@gmail.com>
2024-04-15 11:39:27 +0300

    renamed class to Dataset

--- dataset.py
+++ dataset.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import Dataset
 import os
 
-class SkinDataset(Dataset):
+class Dataset(Dataset):
     def __init__(self, root, device):
         self.root = root
         if self.root[-1] != '/':
